a709608153ba4f2c68128278eb2eacae81142146
Vadym Stavnychyi <vadim96dk@gmail.com>
2020-04-22 15:48:52 +0300

    testing structure courseReducer

--- src/redux/reducers/courseReducer.test.js
+++ src/redux/reducers/courseReducer.test.js
@@ -17,7 +17,10 @@ it("should add course when passed CREATE_COURSE_SUCCESS", () => {
 
   const action = actions.createCourseSuccess(newCourse);
   const newState = courseReducer(initialState, action);
-  
+
+  expect(newState[0].title).toEqual("A");
+  expect(newState[1].title).toEqual("B");
+  expect(newState[2].title).toEqual("C");
   expect(newState.length).toEqual(3);
 });
 
@@ -30,7 +33,12 @@ it("should update course when passed UPDATE_COURSE_SUCCESS", () => {
 
   const course = { id: 2, title: "New Title" };
   const action = actions.updateCourseSuccess(course);
-  const newState = courseReducer(initialState, action);
 
+  const newState = courseReducer(initialState, action);
+  const updatedCourse = newState.find(a => a.id === course.id);
+  const untouchedCourse = newState.find(a => a.id === 1);
+
+  expect(updatedCourse.title).toEqual("New Title");
+  expect(untouchedCourse.title).toEqual("A");
   expect(newState.length).toEqual(3);
 });
