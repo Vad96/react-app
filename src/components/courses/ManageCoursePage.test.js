ee3d7fc8b32d9faa7243f43eee0ea2d723367f25
Vadym Stavnychyi <vadim96dk@gmail.com>
2020-04-23 23:15:17 +0300

    ManageCoursePage testing click

--- src/components/courses/ManageCoursePage.test.js
+++ src/components/courses/ManageCoursePage.test.js
@@ -3,20 +3,33 @@ import renderer from "react-test-renderer";
 import { authors, courses } from "../../../tools/mockData";
 import { ManageCoursePage } from "./ManageCoursePage";
 
-const props = {
-authors,
-courses,
-history,
-saveCourse: jest.fn(),
-loadAuthors: jest.fn(),
-loadCourses: jest.fn(),
-};
-
-it("should contain form with 3 input fields", () => {
-  const tree = renderer.create(
-      <ManageCoursePage {...props} />
+function render(args) {
+    const defaultProps = {
+      authors,
+      courses,
+      saveCourse: jest.fn(),
+      loadAuthors: jest.fn(),
+      loadCourses: jest.fn(),
+    };
+  
+    const props = { ...defaultProps, ...args };
+  
+    const tree = renderer.create(
+        <ManageCoursePage {...props} />
     );
-  
-    expect(tree).toMatchSnapshot();
+    return tree
+  }
+
+it("should render correctly after clicking on the submit button", () => {
+    const tree = render();
+    const instance = tree.root
+
+    const button = instance.findByProps({
+        type: 'submit',
+    });
+    const eventMock = { preventDefault: jest.fn() };
+    renderer.act(() => button.props.onSubmit(eventMock));
+
+    const snap = tree.toJSON();
+    expect(snap).toMatchSnapshot();
 });
-  
