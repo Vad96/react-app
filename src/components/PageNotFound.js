902e5d426ac495af6014086b82fb0fe5a3b12f87
Vadym Stavnychyi <vadim96dk@gmail.com>
2020-04-24 17:31:55 +0300

    adding flow to first component

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -1,5 +1,12 @@
+//@flow
 import React from "react";
 
-const PageNotFound = () => <h1>Oops! Page not found.</h1>;
-
+class PageNotFound extends React.Component<{}> {
+    constructor() {
+        super();
+    }
+    render() { 
+        return ( <h1>Oops! Page not found.</h1> );
+    }
+}
 export default PageNotFound;
