46c018a1d8ef8dc6d0f9666e5dffb0cbcc229da8
Vadym Stavnychyi <vadim96dk@gmail.com>
2020-05-06 22:22:45 +0300

    adding typeof

--- src/redux/reducers/courseReducer.js
+++ src/redux/reducers/courseReducer.js
@@ -11,27 +11,26 @@ type State = {
     authorId: number,
     category: string,
     authorName: string,
-    course: any
+    course: any,
   }>,
 }
 
-type Action =
-  | { type: 'CREATE_COURSE_SUCCESS', action: Array<State> }
-  | { type: 'UPDATE_COURSE_SUCCESS', action: Array<State> }
-  | { type: 'LOAD_COURSES_SUCCESS', action: Array<State> }
-  | { type: 'DELETE_COURSE_OPTIMISTIC', action: Array<State> };
-  
+type Action = {
+    type: string,
+    action: Array<State> 
+}
+    
 export default function courseReducer(state: Array<State> = initialState.courses, action: Action): Array<State> {
   switch (action.type) {
-    case types.CREATE_COURSE_SUCCESS:
+    case typeof types.CREATE_COURSE_SUCCESS:
       return [...state, { ...action.course }];
-    case types.UPDATE_COURSE_SUCCESS:
+    case typeof types.UPDATE_COURSE_SUCCESS:
       return state.map(course =>
         course.id === action.course.id ? action.course : course
       );
-    case types.LOAD_COURSES_SUCCESS:
+    case typeof types.LOAD_COURSES_SUCCESS:
       return action.courses;
-    case types.DELETE_COURSE_OPTIMISTIC:
+    case typeof types.DELETE_COURSE_OPTIMISTIC:
       return state.filter(course => course.id !== action.course.id);
     default:
       return state;
